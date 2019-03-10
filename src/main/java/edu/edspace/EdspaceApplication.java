f3dea22fe1c1c7017f32f132b5e0160c3275a92e
Jorge Luis Malla Sánchez <jomasloja@gmail.com>
2019-03-10 11:06:54 -0500

    Init

---
+++ src/main/java/edu/edspace/EdspaceApplication.java
@@ -0,0 +1,13 @@
+package edu.edspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdspaceApplication.class, args);
+	}
+
+}
